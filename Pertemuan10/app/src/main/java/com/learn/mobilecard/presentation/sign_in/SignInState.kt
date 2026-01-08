ade323e3d5eac64cf67ff52cbfb603057648397d
AlanKrisnaDifaYana <alandifaa@gmail.com>
2026-01-08 14:33:14 +0700

    Pertemuan10

---
+++ Pertemuan10/app/src/main/java/com/learn/mobilecard/presentation/sign_in/SignInState.kt
@@ -0,0 +1,6 @@
+package com.learn.mobilecard.presentation.sign_in
+
+data class SignInState(
+    val isSignInSuccessfull:Boolean = false,
+    val signInError: String? = null
+)
---
+++ Pertemuan10/app/src/main/java/com/learn/mobilecard/presentation/sign_in/SignInViewModel.kt
@@ -0,0 +1,24 @@
+package com.learn.mobilecard.presentation.sign_in
+
+import androidx.lifecycle.ViewModel
+import com.learn.mobilecard.data.SignInResult
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SignInViewModel: ViewModel() {
+    private val  _state = MutableStateFlow(SignInState())
+    val state = _state.asStateFlow()
+
+    fun onSignInResult(result: SignInResult){
+        _state.update {
+            it.copy(
+                isSignInSuccessfull = result.data != null,
+                signInError = result.errorMessage
+            )
+        }
+    }
+    fun resetState() {
+        _state.update { SignInState() }
+    }
+}
